f94e375badeb8893656093f6725865908c39684b
mateus amelio <mateus_aruana@hotmail.com>
2025-10-22 22:40:33 -0300

    feat: add cli command to read file and read test file tokens

--- main.go
+++ main.go
@@ -1,4 +1,36 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
+	// cli
+	fPath := flag.String("file", "", "-file=<path>")
+	flag.Parse()
+
+	// file
+	f, err := os.Open(*fPath)
+	check(err)
+	defer f.Close()
+
+	// read
+	r := bufio.NewScanner(f)
+	r.Split(bufio.ScanRunes)
+	i := 0
+	for r.Scan() {
+		s := r.Text()
+		rr := []rune(s)[0]
+		fmt.Printf("%q \n", rr)
+		i++
+	}
 }
